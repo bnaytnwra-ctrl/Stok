010eae98ae6cdec6da247eefabe387be9f1d82c6
bnaytnwra-ctrl <bnaytnwra@gmail.com>
2026-09-24 20:00:26 +0300

    إضافة علامة اختبار SEC بعد تحديث المحلل

--- src/config.py
+++ src/config.py
@@ -67,3 +67,5 @@ VOLUME_SPIKE_MULTIPLIER = 3.0
 LOG_CSV_PATH = "logs/alerts_log.csv"
 SEEN_IDS_PATH = "logs/seen_ids.json"
 SEEN_IDS_RETENTION_DAYS = 30
+SEC_RETEST_MARKER_PATH = "logs/sec_retest_v1.done"
+
